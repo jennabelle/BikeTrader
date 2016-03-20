e9a557278ad5fac5a01bf875ed9b3476fc264df2
Jenna Vuong <jennavuong@gmail.com>
2016-03-20 15:25:57 -0700

    (feat) add author to post, add /login and /register routes to server

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-// modules
+// ======================================
+// SETUP
+// ======================================
+
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -8,17 +11,18 @@ var mongoose = require('mongoose');
 var Post = require('./app/models/post.js');
 var multipart = require('connect-multiparty');
 var multipartMiddleWare = multipart();
+var db = require('./config/db.js');
 var passport = require('passport');
 var User = require('./app/models/user.js');
 var passportConfig = require('./config/passport.js');
 var jwt = require('express-jwt');
-
-var auth = jwt({ secret: 'SECRET', userProperty: 'payload' });
-
-// configure and connect to database
-var db = require('./config/db.js');
+var auth = jwt({ secret: 'SECRET', userProperty: 'payload' }); // strongly rec'd to use environment variable to reference secret
 
 var port = process.env.PORT || 8080;
+
+// ======================================
+// CONFIGURATION
+// ======================================
 
 // get all data of POST body
 app.use(bodyParser.json());
@@ -77,7 +81,8 @@ app.post('/api/post', multipartMiddleWare, function(req, res, next) {
     price: req.body.price,
     picFile: req.body.picFile,
     email: req.body.email,
-    dateCreated: req.body.dateCreated
+    dateCreated: req.body.dateCreated,
+    // author: req.payload.username // jenna: need to add 'auth' here first before referenceing payload!
   });
   console.log('\n\n\n newPost', newPost, '\n\n\n');
   // console.log('\n\n\n\n%%%%%%%%%%%%%%%%  req', body, '\n\n\n\n$$$$$$$$$$$$$$');
@@ -85,8 +90,51 @@ app.post('/api/post', multipartMiddleWare, function(req, res, next) {
     if (err) { return next(err); }
     res.json(req.body);
   });
-
 });
+
+// register new user
+app.post('/api/register', function (req, res, next) {
+
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Please fill out all fields.' });
+  }
+
+  var user = new User();
+  user.username = req.body.username;
+  user.setPassword(req.body.password);
+
+  user.save(function (err) {
+    if (err) {
+      return next(err);
+    }
+    return res.json({
+      token: user.generateJWT()
+    });
+  })
+});
+
+// login user
+app.post('/api/login', function (req, res, next) {
+
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({ message: 'Please fill out all fields. '});
+  }
+
+  passport.authenticate('local', function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (user) {
+      return res.json({
+        token: user.generateJWT()
+      });
+    }
+    else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
+
 
 // start app
 app.listen(port);
