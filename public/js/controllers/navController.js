7fb42df1b3f4eee68a81e7010322ef68e74c4c2e
Jenna Vuong <jennavuong@gmail.com>
2016-03-20 22:44:28 -0700

    (cleanup) remove comments

--- public/js/controllers/navController.js
+++ public/js/controllers/navController.js
@@ -4,17 +4,4 @@ angular.module('navCtrl', [])
   $scope.isLoggedIn = authFactory.isLoggedIn;
   $scope.currentUser = authFactory.currentUser;
   $scope.logOut = authFactory.logOut;
-
-  // add location path to current scope
-  // $scope.currentPath = $location.path();
-  // $scope.location = window.location;
-  //
-  // $scope.currentState = false;
-  //
-  // if ($scope.location.hash === '#/post') {
-  //   $scope.currentState = true;
-  // } else {
-  //   $scope.currentState = false;
-  // }
-  // console.log('###############', this);
 }]);
